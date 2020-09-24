bf919550fdd528210933933ffd4822c35ababa3f
kaichi <chibisea@gmail.com>
2020-09-24 04:29:43 +0000

    kadai02

---
+++ 02kadai03.php
@@ -0,0 +1,5 @@
+<?php
+$hello = "Hello,";
+$name = " kaichi";
+$world = "'s Wold!";
+echo $hello . $name . $world;
---
+++ sum.php
@@ -0,0 +1,5 @@
+<?php
+$array = array(2,6,4,0,7,5,1,9);
+arsort($array);
+print_r($array)
+?>
